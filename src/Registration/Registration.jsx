a65fca49cc936c6f745ea9aa0aa62f9247364947
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-06-13 18:54:00 +0600

    setup

---
+++ src/Registration/Registration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h2>Registration Please</h2>
+
+            
+        </div>
+    );
+};
+
+export default Registration;
